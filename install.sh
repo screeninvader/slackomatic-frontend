38bf6d95f4a106b6b7747940f1fa7befa8e0ba6d
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-14 09:19:32 +0100

    rewrote install script

--- install.sh
+++ install.sh
@@ -1,19 +1,8 @@
-#!/bin/bash
+#install node for raspbian
+wget -N http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+rm ./node_latest_armhf.deb
 
-.PHONY: log
-
-node: #install node
-	wget -N http://node-arm.herokuapp.com/node_latest_armhf.deb
-	sudo dpkg -i node_latest_armhf.deb
-	rm ./node_latest_armhf.deb
-
-npm: #install node deps
-	npm install
-	sudo npm install -g babel forever
-	mkdir /home/pi/nodejs/log -p
-
-install: node npm;
-
-run: #start the app
-	supervisor --harmony --extensions "js,jade,styl" \
-		--exec babel-node --experimental --ignore='node_modules' -- src/slack.js
+#install local dependencies, babel and forever
+npm install
+sudo npm install -g babel forever
