f1e3559e5de4d1e707affd01206d2db0d73b78ce
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-17 02:54:47 +0100

    http now has timeout, shows error on connection error

--- public/js/http.js
+++ public/js/http.js
@@ -1,8 +1,27 @@
+import {css, getErrorEle} from './utils';
+
 export function get(url){
-    var xmlHttp = null;
-    console.log(`http request to url: ${url}`);
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, true );
-    xmlHttp.send( null );
-    //we do not care about the return for now
+  var xhr = new XMLHttpRequest()
+  console.log(`http request to url: ${url}`);
+  xhr.timeout = 4000;
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      if ( xhr.status === 500 ) {
+        var errorEle = getErrorEle('API Connect Error');
+        css.toggleClass(document.body, 'connection-error');
+        var timeOut = setTimeout(() => {
+          clearTimeout(timeOut);
+          css.toggleClass(document.body, 'connection-error');
+          getErrorEle('');
+        }, 3000);
+      };
+    }
+  }
+  
+  xhr.ontimeout = () => { 
+    console.log("Timed out!!!"); 
+  }
+
+  xhr.open( "GET", url, true );
+  xhr.send( null );
 }
