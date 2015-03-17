60f7cab1d95f06b6a7a2f40d492fd275d6d5e29b
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-17 02:55:26 +0100

    static now loads html by default, readded jade for dynamic page rendering

--- src/slack.js
+++ src/slack.js
@@ -6,15 +6,22 @@ import api from './lib/api';
 import errorHandler from './lib/errorHandler';
 import logger from './lib/logger';
 import killer from './lib/killer';
+import {view} from './lib/view';
+import info from './public/versions.json';
 
 var slack = express()
   , env       = slack.get('env')
   , cwd       = __dirname
-  , viewDir   = join(cwd, 'views')
+  , staticDir = join(cwd, 'static')
   , publicDir = join(cwd, 'public')
+  , viewDir   = join(cwd, 'views')
 ;
 
 slack.set('port', process.env.PORT || 80);
+
+// view engine setup for the rare cases where no html file exists
+slack.set('views', viewDir);
+slack.set('view engine', 'jade');
 
 slack.use(favicon(join(publicDir, 'favicon.ico')));
 
@@ -24,11 +31,27 @@ slack.use(express.static(publicDir));
 logger(slack);
 
 //slackomatic api redirect
-slack.use('/slackomatic', api);
+slack.use('/slackomatic/*', api);
 
 slack.use(killer);
 
-slack.use(express.static(viewDir));
+slack.use(express.static(staticDir, {
+  extensions: ["html"], //add html extension
+  index: true //always load index.html files on /
+}));
+
+slack.use('/slackomatic', (req, res, next) => {
+  res.locals.info = info;
+});
+
+//renders :page from views/pages
+slack.use('/:page', view);
+
+//renders views/pages/index
+slack.use('/', (req, res, next) => {
+  req.params.page = 'index';
+  view(req, res, next);
+});
 
 /// catch 404 and forwarding to error handler
 slack.use((req, res, next) => {
