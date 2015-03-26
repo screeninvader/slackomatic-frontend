68f04c424fad13c7bda8c82b69b767bdc8af13d6
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-26 05:03:35 +0100

    removed server

--- server/lib/killer.js
+++
@@ -1,9 +0,0 @@
-import {Router as router} from 'express';
-var killer = router();
-
-killer.get('/killkillkill', (req, res, next) => {
-  res.status(200).send('killed process');
-  process.exit(0);
-});
-
-export default killer;
--- server/lib/view.js
+++
@@ -1,7 +0,0 @@
-export function view(req, res, next) {
-  res.render('pages/' + req.params.page, (err, html) => {
-    if ( err || ! html ) { next(err); }
-
-    res.status(200).send(html);
-  });
-}
