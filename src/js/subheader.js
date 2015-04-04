23a4fd1f7becf57adf50b84fe8ef063b6a5893a4
Jascha Ehrenreich <jascha@jaeh.at>
2015-04-05 01:51:22 +0200

    fixed subheaders, simplified html, changed logo

--- src/js/subheader.js
+++ src/js/subheader.js
@@ -3,21 +3,39 @@ import {each} from 'magic-loops';
 
 var hack = document.documentElement.doScroll
   , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(document.readyState)
+  , animDuration = 5000
+  , interval
+  , containerDiv = document.querySelector('header.main div.sub')
+  , visibleClassName = 'visible'
+  , intervalRunning = false;
 ;
 
-if ( ! loaded ) {
-  document.addEventListener('DOMContentLoaded', addInterval);
-} else {
-  addInterval();
+if ( window.innerWidth >= 320 ) {
+  if ( ! loaded ) {
+    document.addEventListener('DOMContentLoaded', addInterval);
+  } else {
+    addInterval();
+  }
 }
 
 function addInterval() {
-  showHide();
-  setInterval( showHide, 5000 );
+  if ( intervalRunning === false ) {
+    console.log('addInterval');
+    intervalRunning = true;
+    interval = setInterval( showHide, animDuration );
+  }
+}
+
+function removeInterval() {
+   if ( intervalRunning ) {
+    console.log('removeInterval');
+    intervalRunning = false;
+    clearInterval(interval);
+  }
 }
 
 function setup() {
-  var lis = document.querySelectorAll('header.main div.sub li')
+  var lis = containerDiv.querySelectorAll('li')
     , replacers = [ 
         { val: 'o', regex: /o/g } 
       , { val: 'O', regex: /O/g }
@@ -43,21 +61,42 @@ function setup() {
 setup();
 
 function showHide() {
-  var shown = document.querySelectorAll('header.main div.sub li.visible')
-    , hidden = document.querySelectorAll('header.main div.sub li:not(.visible)')
-    , ran = Math.floor(Math.random() * ( hidden.length - 0.01 ) )
-    , showNow = hidden[ran]
-  ;
+  if ( window.innerWidth >= 320 ) {
+    var hidden = containerDiv.querySelectorAll('li:not(.visible)')
+      , ran = Math.floor(Math.random() * ( hidden.length - 0.01 ) )
+      , showNow = hidden[ran]
+    ;
 
-  each(shown, (li) => {
-    if ( li.classList && isF(li.classList.remove) ) {
-      li.classList.remove('visible');
+    hideAll();
+
+    if ( showNow && showNow.classList && isF(showNow.classList.add) ) {
+      showNow.classList.add(visibleClassName);
     }
-  });
-
-  if ( showNow && showNow.classList && isF(showNow.classList.add) ) {
-    showNow.classList.add('visible');
   }
 }
 
+function hideAll() {
+  var shown = containerDiv.querySelectorAll('li.visible'); 
+  each(shown, (li) => {
+    if ( li.classList && isF(li.classList.remove) ) {
+      li.classList.remove(visibleClassName);
+    }
+  });
+}
 
+function showFirstChild() {
+  var child = containerDiv.querySelector('li:first-child');
+  hideAll();
+  if ( child && child.classList && isF(child.classList.add) ) {
+    child.classList.add(visibleClassName);
+  }
+}
+
+window.addEventListener('resize', () => {
+  if ( window.innerWidth < 320 ) {
+    showFirstChild();
+    removeInterval();
+  } else {
+    addInterval();
+  }
+});
