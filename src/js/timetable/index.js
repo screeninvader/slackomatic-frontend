d5b0400b9dd3ca0521ffb3dec06fd68778ffa263
Jascha Ehrenreich <jascha@jaeh.at>
2015-04-10 19:47:50 +0200

    added on/off indicators to btns

--- src/js/timetable/index.js
+++ src/js/timetable/index.js
@@ -12,60 +12,135 @@ class Timetable {
     , '2': 'tram'
     };
     this.shownLines = [];
-    
-    this.addAllAndNoneBtns();
-    this.initShowLines();
+
+    this.init();
 
     this.departures = [];
     this.socket = new Socket(this);
 
   }
 
-  initShowLines() {
+  init() {
     var selectorEle = document.querySelector('ul#selectors')
       , lis = []
     ;
+    this.addAllAndNoneBtns();
+
     if ( selectorEle ) {
       each(this.supportedLines, (linie, name) => {
-        var li = document.createElement('li');
+        var li = document.createElement('li')
+          , a  = document.createElement('a')
+        ;
         li.className = `linie-${name} ${linie.type} i-${linie.type}`;
         li.style['background-color'] = linie.color;
         li.addEventListener('click', this.toggleSelectedLine.bind(this));
-        li.innerText = name;
+        a.innerText = name;
+        a.className = 'i i-cancel';
         li.setAttribute('data-line', name);
+        li.appendChild(a);
 
         selectorEle.appendChild(li);
       });
     }
+    
+    this.setButtonClasses();
   }
-  
+
   addAllAndNoneBtns() {
     var targetDiv = document.querySelector('ul#selectors')
       , allBtn  = document.createElement('li')
       , noneBtn = document.createElement('li')
+      , allA   = document.createElement('a')
+      , noneA   = document.createElement('a')
     ;
-    allBtn.innerHTML = 'ALL';
-    noneBtn.innerHTML = 'NONE';
-    allBtn.addEventListener( 'click', this.showAllLines.bind(this) );
-    noneBtn.addEventListener( 'click', this.showNoLines.bind(this) );
-    
+    allA.innerHTML = 'ALL';
+    noneA.innerHTML = 'NONE';
+    allA.className = 'i-cancel';
+    noneA.className = 'i-cancel';
+    allBtn.appendChild(allA);
+    noneBtn.appendChild(noneA);
+
+    allBtn.addEventListener( 'click', this.showAllLines.bind(this), true );
+    noneBtn.addEventListener( 'click', this.showNoLines.bind(this), true );
+
     targetDiv.appendChild(allBtn);
     targetDiv.appendChild(noneBtn);
   }
-  showAllLines() {
-    var newLines = [];
+
+  showAllLines(evt) {
+    var targetUl = document.querySelector('ul#selectors')
+      , btns     = targetUl.querySelectorAll('li a')
+      , newLines = []
+    ;
+
     each(this.supportedLines, (line, name) => {
       newLines.push(name);
     });
     this.shownLines = newLines;
-    this.rerender();
+    this.render();
   }
 
-  showNoLines() {
+  showNoLines(evt) {
+    var targetUl = document.querySelector('ul#selectors')
+      , btns     = targetUl.querySelectorAll('li a')
+    ;
     this.shownLines = [];
-    this.rerender();
+    this.render();
   }
 
+  setButtonClasses() {
+    var ul   = document.querySelector('ul#selectors')
+      , btns = ul.querySelectorAll('li')
+      , as   = ul.querySelectorAll('li a')
+      , shownLines = this.shownLines
+      , all  = false
+      , none  = false
+    ;
+    console.log('shownLines', shownLines);
+    each(as, (a) => {
+      if ( count(shownLines) >= count(this.supportedLines) ) {
+        if ( a.innerText !== 'NONE' ) {
+          this.btnOn(a);
+        } else {
+          this.btnOff(a);
+        }
+      } else if ( count(shownLines) === 0) {
+        if ( a.innerText === 'NONE' ) {
+          this.btnOn(a);
+        } else {
+          this.btnOff(a);
+        }
+      } else if ( a.innerText ) {
+        if ( a.innerText === 'NONE' || a.innerText === 'ALL' ) {
+          this.btnOff(a);
+        } else if ( shownLines.indexOf(a.innerText) > -1 ) {
+          this.btnOn(a);
+        } else {
+          this.btnOff(a);
+        }
+      }
+    });
+    
+  }
+  btnOn(a) {
+    if ( a && a.classList && isF(a.classList.remove) ) {
+      a.classList.add('i-ok');
+      a.classList.remove('i-cancel');
+    }
+  }
+
+  btnOff(a) {
+    if ( a && a.classList && isF(a.classList.remove) ) {
+      a.classList.remove('i-ok');
+      a.classList.add('i-cancel');
+    }
+  }
+  btnToggle(a) {
+    if ( a && a.classList && isF(a.classList.toggle) ) {
+      a.classList.toggle('i-ok');
+      a.classList.toggle('i-cancel');
+    }
+  }
   sortByDepartTime(lines) {
     var times = [];
 
@@ -81,7 +156,7 @@ class Timetable {
 
   update(departures) {
     this.departures = departures;
-    this.rerender();
+    this.render();
   }
 
   getLines(cb) {
@@ -105,20 +180,18 @@ class Timetable {
     });
   }
 
-  rerender() {
+  render() {
     var targetUl = document.querySelector('#departures')
       , departures = this.sortByDepartTime(this.departures)
       , shownLineNum = 0
     ;
     targetUl.innerHTML = '';
     each(departures, (depByMinute, minute) => {
-      console.log('minute', minute);
       each(depByMinute, (dep, depKey) => {
         var linie = this.showDeparture(dep)
           , isShown = this.shownLines.indexOf(linie) > -1
         ;
         if ( isShown ) {
-          console.log(isShown, dep);
           shownLineNum += 1;
         }
       });
@@ -127,6 +200,8 @@ class Timetable {
       this.betriebsschluss(targetUl);
     }
     this.departures = departures;
+
+    this.setButtonClasses();
   }
 
   betriebsschluss(target) {
@@ -173,8 +248,12 @@ class Timetable {
   }
 
   toggleSelectedLine(evt) {
-    var shownLines = this.shownLines
-      , toggledLine = evt.target.getAttribute('data-line')
+    var target = evt.target.parentNode.tagName === 'LI'
+                  ? evt.target.parentNode
+                  : evt.target
+      , shownLines = this.shownLines
+      , toggledLine = target.getAttribute('data-line')
+      , childA     = target.querySelector('a')
       , newLines = []
       , found = false
     ;
@@ -188,7 +267,7 @@ class Timetable {
     if ( ! found ) {
       newLines.push(toggledLine);
     }
-    this.setLines(newLines, this.rerender.bind(this));
+    this.setLines(newLines, this.render.bind(this));
   }
 }
 
